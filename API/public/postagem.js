382390a8d4c4c5d6fa610c0558a5e92cca311b79
BrunnoCastigrini <brunno.castigrini@bandtec.com.br>
2021-05-23 22:54:30 -0300

    Trabalho bem feito

--- API/public/postagem.js
+++ API/public/postagem.js
@@ -1,32 +1,9 @@
 function show_hide() {
-    btn_criar.style.display='none';
-    div_criar.style.display='block';
-    
+    btn_criar.style.display = 'none';
+    div_criar.style.display = 'block';
+
 }
 
-// function postar(){
-//     var titulo = in_titulo.value;
-//     var url = in_url.value;
-//     var texto = nova_postagem.value;
-
-//     var valido = texto == "" || titulo == "";
-    
-//     if (valido) {
-//         alert(`Hey, você esqueceu de colocar um título ou uma descrição para a sua postagem!`)
-
-//     } else {
-//         if (url == "") {
-            
-//         } else {
-
-            
-//         }
-
-//         document.location.reload(true);
-        
-//     }
-    
-// }
 
 function contagem() {
     var campo_texto = nova_postagem.value;
@@ -34,3 +11,15 @@ function contagem() {
 
     b_tamanho.innerHTML = tamanho;
 }
+
+
+// function publicar() {
+//      var titulo = in_titulo.value;
+// var texto = nova_postagem.value;
+
+// var invalido = texto == "" || titulo == "";
+
+// if (invalido) {
+//     alert(`Hey, você esqueceu de colocar um título ou uma descrição para a sua postagem!`)
+
+// } else { }
